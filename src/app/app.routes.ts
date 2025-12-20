4b98a148ed82c98de152a7f7bd91582386b268aa
Artem <artemsavcenko963@gmail.com>
2025-12-20 16:17:09 +0100

    Change src/app/pages to src/app/components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes } from '@angular/router';
-import { AllProducts } from './pages/all-products/all-products';
-import { ProductDetails } from './pages/products-details/products-details';
+import { AllProducts } from './components/all-products/all-products';
+import { ProductDetails } from './components/products-details/products-details';
 
 export const routes: Routes = [
   { path: '', component: AllProducts },
